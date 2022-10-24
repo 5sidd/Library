f5ed18359fc548cbfd661d46276acf4aa2b1063f
Ryan Talbot <ryan.talbot@evolvemortgageservices.com>
2022-10-24 17:05:26 -0500

    Add book grid

--- Website/LibraryWebsite/app.js
+++ Website/LibraryWebsite/app.js
@@ -18,6 +18,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use("/jquery", express.static(path.join(__dirname, "/node_modules/jQuery/dist")));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
