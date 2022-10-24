f3fdf50d187443d55791a88ff00600981797e9d9
5sidd <siddpcal@gmail.com>
2022-10-24 12:57:26 -0500

    Search functionality complete

--- backend/app.js
+++ backend/app.js
@@ -27,6 +27,8 @@ app.get('/books', async (req, res) => {
         let q = 'select CD.Isbn, Title, Author_id, `Name`, Loan_id from (select Isbn, Title, BC.Author_id, `Name` from (select BOOK.Isbn, Title, AB.Author_id from BOOK_AUTHORS as AB join BOOK on AB.Isbn = BOOK.Isbn) as BC join AUTHORS on BC.Author_id = AUTHORS.Author_id) as CD left join BOOK_LOANS on CD.Isbn = BOOK_LOANS.Isbn';
         let w = 'WHERE';
 
+        console.log(isbn, title, author);
+
         if (isbn) {
             if (w === 'WHERE') {
                 w += ` Isbn = ${isbn}`
@@ -54,7 +56,11 @@ app.get('/books', async (req, res) => {
             }
         }
 
-        q += w;
+        if (w !== 'WHERE') {
+            q += ' ';
+            q += w;
+        }
+
         connection.query(q, (error, results, fields) => {
             if (error) {
                 return res.status(500).json({ error });
@@ -69,7 +75,7 @@ app.get('/books', async (req, res) => {
     }
 });
 
-//Get book by ISBN number
+/*
 app.get('/books/:id', async (req, res) => {
     try {
         let isbn = req.params.id;
@@ -97,8 +103,50 @@ app.get('/books/:id', async (req, res) => {
         res.status(500).json({ error });
     }
 });
+*/
 
-//Borrower Management
+//Add new borrower to the database --> STILL IN PROGRESS
+app.post('/borrowers', async (req, res) => {
+    try {
+        const { name, ssn, address, phone } = req.body;
+
+        if (/^[a-zA-Z() ]+$/.test(name) === False) {
+            return res.status(400).json({ error: 'Name can only contain alphabetical letters'});
+        }
+
+        if (name.length > 100) {
+            name = name.subtring(0, 100);
+        }
+
+        if (ssn.length === 11) {
+            if (/[0-9]{3}-[0-9]{3}-[0-9]{4}/.test(ssn) === false) {
+                return res.status(400).json({ error: 'Invalid SSN' });
+            }
+        }
+        else {
+            return res.status(400).json({ error: 'Invalid SSN' });
+        }
+
+        if (address.length > 200) {
+            address = address.substring(0, 100);
+        }
+
+        if (phone.length === 10) {
+            if (/[0-9]{10}/.test(phone) === false) {
+                return res.status(400).json({ error: 'Invalid phone number'});
+            }
+        }
+        else {
+            return res.status(400).json({ error: 'Invalid phone number'});
+        }
+
+
+    }
+    catch (error) {
+        console.log(error);
+        res.status(500).json({ error });
+    }
+});
 
 const port = process.env.PORT || 3000;
 const start = () => {
