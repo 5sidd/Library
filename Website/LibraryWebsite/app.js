d55b9fa690f4822cdaf9bba48db8e4ff64129f37
Ryan Talbot <ryan.talbot@evolvemortgageservices.com>
2022-11-04 19:13:03 -0500

    Git rid of users router

--- Website/LibraryWebsite/app.js
+++ Website/LibraryWebsite/app.js
@@ -5,7 +5,6 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
 var app = express();
 
@@ -22,7 +21,6 @@ app.use("/jquery", express.static(path.join(__dirname, "/node_modules/jQuery/dis
 app.use("/semantic/dist", express.static(path.join(__dirname, "/node_modules/semantic-ui/")));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
